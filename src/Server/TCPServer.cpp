4f3524885395fc4245df638cdc3e585e42fe73ce
Saarthak Arora <saarthakarora2002@gmail.com>
2025-08-05 13:12:14 +0000

    optimized for recieving large sized request

--- src/Server/TCPServer.cpp
+++ src/Server/TCPServer.cpp
@@ -48,49 +48,45 @@ void TCPServer::start(int port_) {
             continue;
         }
 
-        char buffer[8192] = {0};  // Increased buffer size
+        const size_t CHUNK_SIZE = 16384; // 16KB per read
         std::string request;
-        ssize_t total_bytes_read = 0;
         ssize_t bytes_read;
-        
-        // Read the request in chunks
-        while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
+        long expected_content_length = -1;
+        size_t header_end = std::string::npos;
+        bool headers_parsed = false;
+
+        while (true) {
+            char buffer[CHUNK_SIZE];
+            bytes_read = read(client_socket, buffer, CHUNK_SIZE);
+            if (bytes_read <= 0) break;
             request.append(buffer, bytes_read);
-            total_bytes_read += bytes_read;
-            
-            // Check if we have received the complete HTTP headers
-            size_t header_end = request.find("\r\n\r\n");
-            if (header_end != std::string::npos) {
-                // Found end of headers, now check if we need to read more data
-                size_t content_length_pos = request.find("Content-Length: ");
-                if (content_length_pos != std::string::npos) {
-                    size_t length_start = content_length_pos + 16;
-                    size_t length_end = request.find("\r\n", length_start);
-                    std::string length_str = request.substr(length_start, length_end - length_start);
-                    long expected_content_length = std::stol(length_str);
-                    
-                    // Calculate how much body we have received
-                    size_t headers_size = header_end + 4;
-                    size_t body_size = request.size() - headers_size;
-                    
-                    std::cout << "Expected Content-Length: " << expected_content_length << " bytes\n";
-                    std::cout << "Body received so far: " << body_size << " bytes\n";
-                    
-                    // If we have received all the expected content, break
-                    if (body_size >= expected_content_length) {
-                        break;
+
+            if (!headers_parsed) {
+                header_end = request.find("\r\n\r\n");
+                if (header_end != std::string::npos) {
+                    size_t content_length_pos = request.find("Content-Length: ");
+                    if (content_length_pos != std::string::npos) {
+                        size_t length_start = content_length_pos + 16;
+                        size_t length_end = request.find("\r\n", length_start);
+                        std::string length_str = request.substr(length_start, length_end - length_start);
+                        expected_content_length = std::stol(length_str);
+                        std::cout << "Expected Content-Length: " << expected_content_length << " bytes\n";
                     }
-                } else {
-                    // No Content-Length header, assume request is complete
+                    headers_parsed = true;
+                }
+            }
+
+            if (headers_parsed && expected_content_length != -1) {
+                size_t headers_size = header_end + 4;
+                size_t body_size = request.size() - headers_size;
+                std::cout << "Body received so far: " << body_size << " bytes\n";
+                if (body_size >= (size_t)expected_content_length) {
                     break;
                 }
             }
-            
-            // Clear buffer for next read
-            memset(buffer, 0, sizeof(buffer));
         }
 
-        if (total_bytes_read > 0) {
+        if (!request.empty()) {
             std::cout << "Total request size: " << request.size() << " bytes\n";
             RequestHandler requestHandler(base_path);
             requestHandler.handleClientRequest(client_socket, request, indexer);
